bb496acc257d852d11a87774a3f66755dea1cf6a
Thomas Leclerc <thomas@MacBook-Pro-de-Thomas.local>
2019-10-07 14:47:27 +0200

    New name

---
+++ td00/src/main/java/fr/dauphine/ja/leclercthomas/td00/App.java
@@ -0,0 +1,13 @@
+package fr.dauphine.ja.leclercthomas.td00;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! Version 3" );
+    }
+}
